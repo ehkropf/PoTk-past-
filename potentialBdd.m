32edcef5d7027438698d7e8828f938b789d2f908
Everett Kropf <ehkropf@gmail.com>
2015-12-14 18:01:36 +0000

    Setup and calculation methods.

--- potentialBdd.m
+++ potentialBdd.m
@@ -26,7 +26,56 @@ methods
 end
 
 methods(Access=protected)
-    function W = setupCirculation(W)
+    function w = calcBdryCirc(W, zeta)
+        % Circulation on the boundaries.
+        
+        w = complex(zeros(size(zeta)));
+        
+        gammav = W.theDomain.islandCirculation(2:end);
+        for j = find(gammav')
+            w = w + gammav(j)*W.vjfuns{j}(zeta);
+        end
+    end
+    
+    function w = calcPotential(W, zeta)
+        % Combine potential flows.
+        
+        w = calcUniformFlow(W, zeta) ...
+            + calcVortexCirc(W, zeta) ...
+            + calcBdryCirc(W, zeta);
+    end
+    
+    function w = calcUniformFlow(W, zeta)
+        % Uniform flow potential.
+        
+        w = complex(zeros(size(zeta)));
+        if isempty(W.beta) || W.theDomain.uniformStrength == 0
+            return
+        end
+        
+        % Do something here. Superclass method?
+    end
+    
+    function w = calcVortexCirc(W, zeta)
+        % Point vortex circulation.
+        
+        w = complex(zeros(size(zeta)));
+        
+        Gammav = W.theDomain.vortexCirculation;
+        for k = find(Gammav')
+            w = w + Gammav(k)*W.g0funs{k}(zeta);
+        end
+    end
+    
+    function W = constructPotential(W)
+        % Call all the setup methods. In the proper order.
+        
+        W = setupBdryCirc(W);
+        W = setupVortexCirc(W);
+        W = setupUniformFlow(W);
+    end
+
+    function W = setupBdryCirc(W)
         % Construct the first kind integrals for boundary circulation
         % computation.
         % Construct them all regardless; then use to construct prime
@@ -41,18 +90,27 @@ methods(Access=protected)
         end
     end
     
-    function W = setupVortices(W)
+    function W = setupUniformFlow(W)
+        % Uniform flow depends on existence of point beta.
+        
+        if isempty(W.beta) || W.theDomain.uniformStrength == 0
+            return
+        end
+        
+        % Do something here? Superclass method?
+    end
+    
+    function W = setupVortexCirc(W)
         % Construct Green's functions for vortices.
         % Extra boundary circulation goes to C0.
         
         alphav = W.theDomain.vortexLocation;
-        Gammav = W.theDomain.vortexCirculation;
         n = numel(alphav);
         if n == 0
             return
         end
         
-        for k = find(Gammav')
+        for k = find(W.theDomain.vortexCirculation')
             W.g0funs{k} = greensC0(alphav(k), W.vjfuns{1});
         end
     end
