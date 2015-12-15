8bee90ed914b6b0effad283d6766057521bb4684
Everett Kropf <ehkropf@gmail.com>
2015-12-15 11:18:35 +0000

    Add GNU license.

--- install.m
+++ install.m
@@ -1,7 +1,22 @@
 function install(arg)
 % PoTk install function.
 
-% E. Kropf, 2014
+% Everett Kropf, 2015
+% 
+% This file is part of the Potential Toolkit (PoTk).
+% 
+% PoTk is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% PoTk is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with PoTk.  If not, see <http://www.gnu.org/licenses/>.
 
 if ~(exist('./potool', 'file') == 2 && ...
         exist('./potential', 'file') == 2 && ...
--- tex/annulusConst.m
+++ tex/annulusConst.m
@@ -1,22 +1,22 @@
 function a = annulusConst(d, q)
 %annulusConst gives the self-map to annulus constant.
 
-% E. Kropf, 2015
+% Everett Kropf, 2015
 % 
-% This file is part of SKPrime.
+% This file is part of the Potential Toolkit (PoTk).
 % 
-% SKPrime is free software: you can redistribute it and/or modify
+% PoTk is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
 % 
-% SKPrime is distributed in the hope that it will be useful,
+% PoTk is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
 % 
 % You should have received a copy of the GNU General Public License
-% along with SKPrime.  If not, see <http://www.gnu.org/licenses/>.
+% along with PoTk.  If not, see <http://www.gnu.org/licenses/>.
 
 % Save circle angle.
 k = exp(1i*angle(d));
