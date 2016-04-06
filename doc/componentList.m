b0b12430d39b79ef07fbfb105c489392309cf8b0
Everett Kropf <ehkropf@gmail.com>
2016-04-06 16:09:41 +0100

    Remove outdated "matlab generated" docs.

--- doc/componentList.m
+++
@@ -1,6 +0,0 @@
-%% List of PoTk components
-% The following are the list of components of the PoTk.
-%
-% * <flowRegionDoc.html flowRegion> class
-% * <potentialDoc.html potential> class
-% * <potoolDoc.html potool>
--- doc/potoolDoc.m
+++
@@ -1,2 +0,0 @@
-%% PoTk Interactive Figure
-% This file documents |potool|, the PoTk interactive figure.
